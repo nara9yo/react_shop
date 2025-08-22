9f238aa448a3957686105401a814cbd86cdeaa6f
Daeyoung Han <nara9yo@gmail.com>
2025-08-22 10:18:17 +0900

    BrowserRouter에 basename 속성 추가: 환경 변수에서 BASE_URL을 로드하여 라우팅 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { Toaster } from 'react-hot-toast'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
         <Toaster position="top-center" toastOptions={{ duration: 2500 }} />
       </BrowserRouter>
